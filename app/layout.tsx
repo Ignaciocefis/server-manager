3ae45598dffb4f10f59e89ee23be0c23a762d004
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-06-04 15:04:46 +0200

    feat(app): Añadido componente sonner de shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-app-0 text-gray-app-600`}
       >
         {children}
+        <Toaster richColors expand visibleToasts={3} />
       </body>
     </html>
   );
