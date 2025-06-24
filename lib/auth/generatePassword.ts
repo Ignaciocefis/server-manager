0b5bb42803e925a478c2633509750c1ae00aae04
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-06-24 10:53:35 +0200

    fix(auth): Error de seguridad de Codacy con cadena para crear las contraseñas, desactivado el error

--- lib/auth/generatePassword.ts
+++ lib/auth/generatePassword.ts
@@ -2,6 +2,7 @@ import { PASSWORD_CHARSET } from "./charsets";
 
 export function generateRandomPassword(length = 10) {
   const chars = PASSWORD_CHARSET;
+  // codacy-disable-next-line Security/HardcodedPassword
   let password = "";
   for (let i = 0; i < length; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length));
