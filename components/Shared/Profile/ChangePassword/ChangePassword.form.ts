c595a5717ba82019a9e2b94db5052f89ed0a359a
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-06-26 17:25:03 +0200

    fix(auth): corrección de fallo de identación

--- components/Shared/Profile/ChangePassword/ChangePassword.form.ts
+++ components/Shared/Profile/ChangePassword/ChangePassword.form.ts
@@ -3,9 +3,11 @@ import z from "zod";
 export const formSchema = z.object({
   currentPassword: 
     z.string().min(1, "La contraseña actual es obligatoria"),
-    newPassword: z.string().min(6, "La nueva contraseña debe tener al menos 6 caracteres"),
-    confirmPassword: z.string().min(1, "Debes confirmar la nueva contraseña"),
-  }).refine((data) => data.newPassword === data.confirmPassword, {
-    message: "Las contraseñas no coinciden",
-    path: ["confirmPassword"],
-  });
+  newPassword: 
+    z.string().min(6, "La nueva contraseña debe tener al menos 6 caracteres"),
+  confirmPassword: 
+    z.string().min(1, "Debes confirmar la nueva contraseña"),
+}).refine((data) => data.newPassword === data.confirmPassword, {
+  message: "Las contraseñas no coinciden",
+  path: ["confirmPassword"],
+});
