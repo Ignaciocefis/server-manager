df6ff2f4ebc0f747ae9c1bc34cc0b8ce98907bb8
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-06-28 18:43:25 +0200

    feat(app): Cambios en los props de PageTitle

--- app/(home)/components/PageTitle/PageTitle.types.ts
+++ app/(home)/components/PageTitle/PageTitle.types.ts
@@ -2,15 +2,5 @@ import { ReactNode } from "react";
 
 export interface PageTitleProps {
   title: string;
-  dialogAction?: {
-    label: string;
-    icon?: ReactNode;
-    content: ReactNode | ((props: { closeDialog: () => void }) => ReactNode);
-  };
-  secondaryAction?: {
-    label: string;
-    icon?: ReactNode;
-    onClick: () => void;
-  };
   children?: ReactNode;
 }
