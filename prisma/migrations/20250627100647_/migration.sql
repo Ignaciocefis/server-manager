82c6a2c8724050595fd186012b7345b8bec3a596
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-06-29 10:36:47 +0200

    feat(db): Migraciones de BD para añadir sesrvidor

--- prisma/migrations/20250627100647_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "assignedToId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_assignedToId_fkey" FOREIGN KEY ("assignedToId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
