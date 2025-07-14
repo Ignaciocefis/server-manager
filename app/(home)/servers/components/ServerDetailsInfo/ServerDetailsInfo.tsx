53ff1ced08fae5b9f0bfa632561095fdbc50c5f5
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-07-14 14:10:14 +0200

    feat(server,gpu): Hacer responsive la información de detalles de servidor

--- app/(home)/servers/components/ServerDetailsInfo/ServerDetailsInfo.tsx
+++ app/(home)/servers/components/ServerDetailsInfo/ServerDetailsInfo.tsx
@@ -23,8 +23,8 @@ export const ServerDetailsInfo: React.FC<ServerDetailsInfoProps> = ({
 }) => {
   return (
     <div className="flex justify-center">
-      <div className="flex px-8 py-4 m-4 w-11/12 text-gray-app-100 bg-gray-app-600 rounded-lg">
-        <div className="flex flex-col w-2/3 pr-8 p-4">
+      <div className="flex flex-col md:flex-row px-4 py-4 m-4 w-full md:w-11/12 text-gray-app-100 bg-gray-app-600 rounded-lg">
+        <div className="flex flex-col w-full md:w-2/3 pr-0 md:pr-8 p-4">
           <h1 className="text-2xl font-bold mb-4">
             Detalles del servidor: {server.name}
           </h1>
@@ -32,12 +32,12 @@ export const ServerDetailsInfo: React.FC<ServerDetailsInfoProps> = ({
             <MemoryStick size={16} className="inline mr-2" />
             <span className="font-semibold">RAM: {server.ramGB} GB</span>
           </p>
-          <p className="flex items-center mb-2">
+          <p className="flex items-center mb-4">
             <HardDrive size={16} className="inline mr-2" />
             <span className="font-semibold">Discos: {server.diskCount} GB</span>
           </p>
 
-          <div className="flex flex-col items-center gap-2 mt-4 w-full max-w-xs">
+          <div className="flex flex-col items-center gap-2 mt-4 w-full max-w-xs self-center">
             {server.available && (server.availableGpus ?? 0) > 0 ? (
               <Button className="w-full bg-green-app-500-transparent hover:bg-green-app-500">
                 <CirclePlus size={16} className="mr-2" />
@@ -71,6 +71,7 @@ export const ServerDetailsInfo: React.FC<ServerDetailsInfoProps> = ({
                     ? "Servidor disponible"
                     : "Servidor en mantenimiento"}
                 </Button>
+
                 <Button
                   className="w-full bg-red-app-500-transparent hover:bg-red-app-500 text-white"
                   onClick={onDelete}
@@ -84,7 +85,7 @@ export const ServerDetailsInfo: React.FC<ServerDetailsInfoProps> = ({
         </div>
 
         <div
-          className={`${(server.gpus?.length ?? 0) > 2 ? "w-1/2" : "w-1/3"} rounded-lg p-4 flex flex-col gap-4`}
+          className={`w-full md:${(server.gpus?.length ?? 0) > 2 ? "w-1/2" : "w-1/3"} rounded-lg p-4 flex flex-col gap-4`}
         >
           <h2 className="text-xl font-semibold mb-2">GPUs instaladas</h2>
           {server.gpus && server.gpus.length > 0 ? (
