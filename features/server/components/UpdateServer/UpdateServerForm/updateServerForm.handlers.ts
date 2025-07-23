dc93d211466a06d7caf1d2637207346e81c87dcd
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-07-23 12:46:17 +0200

    refactor(server): Refactorización de componentes llamada a api en UpdateServerForm

--- features/server/components/UpdateServer/UpdateServerForm/updateServerForm.handlers.ts
+++ features/server/components/UpdateServer/UpdateServerForm/updateServerForm.handlers.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { z } from "zod";
 import { updateServerFormSchema } from "@/features/server/shemas";
+import { toast } from "sonner";
 
 export const updateServer = async (
   data: z.infer<typeof updateServerFormSchema>
 ) => {
-  try {
-    const response = await axios.put("/api/server/update", data);
-    return response.data;
-  } catch (error: unknown) {
-    console.error("Error actualizando servidor:", error);
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.response?.data?.error || "Error desconocido");
+  await axios.put("/api/server/update", data)
+  .then((res) => {
+    if(!res.data.success) {
+      console.error("Error updating server:", res.data.error);
+      toast.error(res.data.error || "Error al actualizar el servidor");
     }
-    throw new Error("Error de red o inesperado");
-  }
+    return res.data.data;
+  }).catch((error) => {
+    console.error("Error updating server:", error);
+    toast.error("Error al actualizar el servidor");
+  });
 };
