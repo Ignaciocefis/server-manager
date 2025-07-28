466bc622ed175720a2154a5ea6e43cab14909147
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-07-28 12:17:23 +0200

    fix(user): Cambios en la llamada a la api para obtener los investigadores

--- features/user/components/AssignResearcherPopover/useAssignResearcherPopover.ts
+++ features/user/components/AssignResearcherPopover/useAssignResearcherPopover.ts
@@ -5,14 +5,23 @@ import { toast } from "sonner";
 
 export function useResearchers(open: boolean) {
   const [researchers, setResearchers] = useState<Researcher[]>([]);
-  
+
   useEffect(() => {
     if (!open) return;
 
     axios
-      .get("/api/researcher/allResearchers")
-      .then((res) => setResearchers(res.data.data || []))
-      .catch(() => toast.error("Error al cargar investigadores"));
+      .get("/api/user/researcher/allResearchers")
+      .then((res) => {
+        if (!res.data.success) {
+          toast.error(res.data.error || "Error al cargar investigadores");
+          return;
+        }
+        setResearchers(res.data.data || []);
+      })
+      .catch((error) => {
+        console.error("Error al cargar investigadores: ", error);
+        toast.error(error.response?.data.error || "Error al cargar investigadores");
+      });
   }, [open]);
 
   return researchers;
