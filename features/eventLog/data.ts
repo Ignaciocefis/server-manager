26df16a3cf1d18055962b40249d97c96555890c7
Ignaciocefis <ignaciocefis2001@gmail.com>
2025-08-21 11:30:56 +0200

    fix(event_log): Eliminación por error de función para crear eventos

--- features/eventLog/data.ts
+++ features/eventLog/data.ts
@@ -1,8 +1,28 @@
 import { db } from "@/lib/db";
 import { ApiResponse } from "@/lib/types/BDResponse.types";
-import { GetLogsParams, LogsTableDataProps } from "./types";
+import { EventLog, GetLogsParams, LogsTableDataProps } from "./types";
 import { getFullName } from "../user/utils";
 import { EventType, Prisma } from "@prisma/client";
+import { eventFormSchema } from "./schemas";
+import z from "zod";
+
+export const createEventLog = async (data: z.infer<typeof eventFormSchema>): Promise<ApiResponse<EventLog | null>> => {
+  try {
+    await db.eventLog.create({
+      data: {
+        userId: data.userId ?? null,
+        serverId: data.serverId ?? null,
+        reservationId: data.reservationId ?? null,
+        eventType: data.eventType,
+        message: data.message,
+      }
+    });
+    return { success: true, data: null, error: null };
+  } catch (err) {
+    console.error("Error creating EventLog:", err);
+    return { success: false, data: null, error: "Error creating EventLog" };
+  }
+};
 
 export const getAllLogs = async (
   params?: GetLogsParams,
